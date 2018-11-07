d6798becd31da5df5caa46bc859dd4e291814c0b
alexander-chashkov <asdsacha@mail.ru>
2018-11-07 21:51:11 +0500

    1.3. JavaDoc. Code convention[#83031]

--- chapter_001/src/main/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,17 @@
 package ru.job4j.calculate;
+/**
+*	@author Alexander Chashkov
+*	@since 2018.11.07
+*	@version 0.0.0.1
+*/
 public class Calculate {
 	public String echo (String value) {
 		return String.format("%s %s %s", value, value, value);
 	}
+	/**
+	* main
+	* @param args - args
+	*/
 	public static void main (String []args){
 		Calculate calc = new Calculate ();
 		System.out.println(calc.echo("aah"));
---
+++ chapter_001/src/main/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alexander Chashkov (ozzyzulka@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
