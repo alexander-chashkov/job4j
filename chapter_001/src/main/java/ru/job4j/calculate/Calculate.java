d888d31ecd2907e30a8ec52d2b62ceb90b10f9ac
alexander-chashkov <asdsacha@mail.ru>
2018-11-10 23:21:32 +0500

    2.5. Инспектор кода IDEA.[#82991]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,22 +1,22 @@
 package ru.job4j.calculate;
 /**
-*	@author Alexander Chashkov
-*	@since 2018.11.07
-*	@version 0.0.0.1
-*/
+ *	@author Alexander Chashkov
+ *	@since 2018.11.07
+ *	@version 0.0.0.1
+ */
 public class Calculate {
 	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}
 	/**
-	* main
-	* @param args - args
-	*/
+	 * main
+	 * @param args - args
+	 */
 	public static void main(String[] args) {
 		Calculate calc = new Calculate();
 		System.out.println(calc.echo("Alexander Chashkov"));
