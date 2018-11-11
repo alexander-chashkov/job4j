d7f14d9245d01fea7555ed7003bb5152a3ed41ae
alexander-chashkov <asdsacha@mail.ru>
2018-11-11 23:12:38 +0500

    3.1. Элементарный калькулятор.[#83022]Добавлены джавадоки

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package ru.job4j.calculator;
 
 /**
+ * Simple Calculator
  * @author Alexander Chashkov
  * @version 0.0.0.1
  * @since 2018.11.11
@@ -8,25 +9,51 @@ package ru.job4j.calculator;
 public class Calculator {
     private double result;
 
+    /**
+     * constructor
+     */
     public Calculator() {
     }
 
+    /**
+     * addition
+     * @param first addend
+     * @param second addend
+     */
     public void add(double first, double second) {
         this.result = first + second;
     }
 
+    /**
+     * subtraction
+     * @param first minuend
+     * @param second subtrahend
+     */
     public void subtract(double first, double second) {
         this.result = first - second;
     }
 
+    /**
+     * multiplication
+     * @param first multiplicand
+     * @param second multiplier
+     */
     public void multiple(double first, double second) {
         this.result = first * second;
     }
 
+    /**
+     * division
+     * @param first dividend
+     * @param second divider
+     */
     public void div(double first, double second) {
         this.result = first / second;
     }
 
+    /**
+     * @return result
+     */
     public double getResult() {
         return this.result;
     }
