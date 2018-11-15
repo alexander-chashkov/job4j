38e20e490a2b1abcfb4fd784d1ee0af935288dc7
alexander-chashkov <asdsacha@mail.ru>
2018-11-15 21:55:16 +0500

    4.1. Глупый бот.[#82994]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+/**
+ * Глупый бот
+ * @author Alexander Chashkov
+ * @version 0.0.0.1
+ * @since 2018.11.15
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     *
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
