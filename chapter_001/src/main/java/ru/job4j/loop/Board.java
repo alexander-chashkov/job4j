747a8a42843e20e7d1880069afa52d02ee92bc2b
alexander-chashkov <asdsacha@mail.ru>
2018-11-19 22:43:39 +0500

    5.3. Построить шахматную доску в псевдографике.[#83006]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+/**
+ * Board
+ * @author Alexander Chashkov
+ * @version 0.0.0.1
+ * @since 2018.11.19
+ */
+public class Board {
+    /**
+     * @param width ширина доски
+     * @param height высота доски
+     * @return рзметка доски
+     */
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if ((i + j) % 2 == 0) {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * BoardTest тестирование
+ * @author Alexander Chashkov
+ * @version 0.0.0.1
+ * @since 2018.11.19
+ */
+public class BoardTest {
+    /**
+     * 3x3 board test
+     */
+    @Test
+    public void when3x3() {
+        Board board = new Board();
+        String rsl = board.paint(3, 3);
+        String ln = System.lineSeparator();
+        assertThat(rsl, is(
+                String.format("X X%s X %sX X%s", ln, ln, ln)
+                )
+        );
+    }
+    /**
+     * 5x4 board test
+     */
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        Board board = new Board();
+        String rsl = board.paint(5, 4);
+        String ln = System.lineSeparator();
+        assertThat(rsl, is(
+                String.format("X X X%s X X %sX X X%s X X %s", ln, ln, ln, ln)
+                )
+        );
+    }
+}
