4aeab0e72839f3c0370b6cc77681d5237a7fd158
alexander-chashkov <asdsacha@mail.ru>
2018-11-26 19:28:22 +0500

    Вопросы.[#83038][#91879]испрвления

--- chapter_001/src/main/java/ru/job4j/array/Combiner.java
+++ chapter_001/src/main/java/ru/job4j/array/Combiner.java
@@ -1,22 +1,20 @@
 package ru.job4j.array;
 
-
 /**
  * Тестовое задание к Вопросы.[#83038][#91879]
  * Объдинить 2 остортированных массива, получить отсортированный массив
- *  @version 0.0.0.1
- *  @author Chashkov Alexander
- *  @since 2018.11.25
+ * @author Chashkov Alexander
+ * @version 0.0.0.1
+ * @since 2018.11.25
  */
 public class Combiner {
     /**
-     * @param arrayFirst отс. массив 1
-     * @param arraySecond отс. массив 2
-     * @return объединение двух отс. массивов, тоже отс. массив
+     * @param arrayFirst  отсортированный массив 1
+     * @param arraySecond отсортированный массив 2
+     * @return объединение двух отсортированных массивов ( отсортированный массив)
      */
     public int[] toCombine(int[] arrayFirst, int[] arraySecond) {
         int[] result = new int[arrayFirst.length + arraySecond.length];
-     //   int length = Math.min(arrayFirst.length, arraySecond.length);
         int k = 0;
         int i = 0;
         int j = 0;
@@ -30,11 +28,8 @@ public class Combiner {
             }
             j++;
         }
-
         System.arraycopy(arrayFirst, i, result, j, arrayFirst.length - i);
         System.arraycopy(arraySecond, k, result, j, arraySecond.length - k);
-
-
         return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/CombinerTest.java
+++ chapter_001/src/test/java/ru/job4j/array/CombinerTest.java
@@ -1,74 +1,79 @@
 package ru.job4j.array;
+
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
 /**
- *  тест Combiner test
- *  @version 0.0.0.1
- *  @author Chashkov Alexander
- *  @since 2018.11.25
+ * тест Combiner test
+ *
+ * @author Chashkov Alexander
+ * @version 0.0.0.1
+ * @since 2018.11.25
  */
 public class CombinerTest {
 
     /**
-     * test 1
+     * test 1 12345 2489 = 122344589
      */
     @Test
     public void whenArr12345Arr2489Then122344589() {
         Combiner combiner = new Combiner();
-        int[] arr1 = new int[] {1, 2, 3, 4, 5};
-        int[] arr2 = new int[] {2, 4, 8, 9};
+        int[] arr1 = new int[]{1, 2, 3, 4, 5};
+        int[] arr2 = new int[]{2, 4, 8, 9};
         int[] rst = combiner.toCombine(arr1, arr2);
         int[] expect = new int[]{1, 2, 2, 3, 4, 4, 5, 8, 9};
         assertThat(rst, is(expect));
     }
 
     /**
-     * test 2
+     * test 2 0 0 = 00
      */
     @Test
     public void whenArr0Arr0Then00() {
         Combiner combiner = new Combiner();
-        int[] arr1 = new int[] {0};
-        int[] arr2 = new int[] {0};
+        int[] arr1 = new int[]{0};
+        int[] arr2 = new int[]{0};
         int[] rst = combiner.toCombine(arr1, arr2);
         int[] expect = new int[]{0, 0};
         assertThat(rst, is(expect));
     }
+
     /**
-     * test 3
+     * test 3 112 1 = 1112
      */
     @Test
     public void whenArr112Arr1Then1112() {
         Combiner combiner = new Combiner();
-        int[] arr1 = new int[] {1, 1, 2};
-        int[] arr2 = new int[] {1};
+        int[] arr1 = new int[]{1, 1, 2};
+        int[] arr2 = new int[]{1};
         int[] rst = combiner.toCombine(arr1, arr2);
         int[] expect = new int[]{1, 1, 1, 2};
         assertThat(rst, is(expect));
     }
 
     /**
-     * test 4
+     * test 4 1589 2234 = 12234589
      */
     @Test
     public void whenArr1589Arr2234Then12234589() {
         Combiner combiner = new Combiner();
-        int[] arr1 = new int[] {1, 5,  8, 9};
-        int[] arr2 = new int[] {2, 2, 3, 4};
+        int[] arr1 = new int[]{1, 5, 8, 9};
+        int[] arr2 = new int[]{2, 2, 3, 4};
         int[] rst = combiner.toCombine(arr1, arr2);
         int[] expect = new int[]{1, 2, 2, 3, 4, 5, 8, 9};
         assertThat(rst, is(expect));
     }
 
     /**
-     * test 5
+     * test 5 2 = 2
      */
     @Test
     public void whenArr2ArrThen2() {
         Combiner combiner = new Combiner();
-        int[] arr1 = new int[] {2};
-        int[] arr2 = new int[] {};
+        int[] arr1 = new int[]{2};
+        int[] arr2 = new int[]{};
         int[] rst = combiner.toCombine(arr1, arr2);
         int[] expect = new int[]{2};
         assertThat(rst, is(expect));
