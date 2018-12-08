586d89064aedc9289e97d343f95585cb2af4cef2
alexander-chashkov <asdsacha@mail.ru>
2018-12-08 15:21:27 +0500

    5. Рефакторинг теста @Before @After[#83066]

--- chapter_002/src/test/java/pseudo/PaintTest.java
+++ chapter_002/src/test/java/pseudo/PaintTest.java
@@ -1,5 +1,7 @@
 package pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -18,42 +20,60 @@ import static org.junit.Assert.assertThat;
  */
 public class PaintTest {
     /**
+     * содержит дефолтный вывод в консоль.
+     */
+    private final PrintStream stdout = System.out;
+
+    /**
+     * буфер для результата.
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    private final String sep = System.lineSeparator();
+
+    /**
+     * устанавливает поток вывода на буффер
+     */
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    /**
+     * устанавливает поток вывода по умлолчанию
+     */
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+    /**
      * test draw square
      */
     @Test
     public void whenDrawSquare() {
-
-        String sep = System.lineSeparator();
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         StringBuilder expected = new StringBuilder();
-        expected.append("█████████").append(sep);
-        expected.append("█           █").append(sep);
-        expected.append("█           █").append(sep);
-        expected.append("█           █").append(sep);
-        expected.append("█           █").append(sep);
-        expected.append("█████████").append(sep).append(sep);
+        expected.append("█████████").append(this.sep);
+        expected.append("█           █").append(this.sep);
+        expected.append("█           █").append(this.sep);
+        expected.append("█           █").append(this.sep);
+        expected.append("█           █").append(this.sep);
+        expected.append("█████████").append(this.sep).append(this.sep);
         assertThat(new String(out.toByteArray()), is(expected.toString()));
-        System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        String sep = System.lineSeparator();
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         StringBuilder expected = new StringBuilder();
-        expected.append("      █").append(sep);
-        expected.append("    █   █").append(sep);
-        expected.append("  █       █").append(sep);
-        expected.append("█████████").append(sep);
-        expected.append(sep);
+        expected.append("      █").append(this.sep);
+        expected.append("    █   █").append(this.sep);
+        expected.append("  █       █").append(this.sep);
+        expected.append("█████████").append(this.sep).append(this.sep);
         assertThat(new String(out.toByteArray()), is(expected.toString()));
-        System.setOut(stdout);
     }
 
 }
