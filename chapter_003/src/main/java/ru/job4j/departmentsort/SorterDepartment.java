529287a74e4c3560666957086434b993d270e88f
alexander-chashkov <asdsacha@mail.ru>
2019-02-09 14:48:47 +0500

    Отсортировать департаменты [#34352][#110542]

--- chapter_003/src/main/java/ru/job4j/departmentsort/SorterDepartment.java
+++ chapter_003/src/main/java/ru/job4j/departmentsort/SorterDepartment.java
@@ -42,11 +42,26 @@ public class SorterDepartment  {
         Set<Department> temp = new TreeSet(new Comparator() {
             @Override
             public int compare(Object o1, Object o2) {
-                return o1.toString().compareTo(o2.toString());
+                int len1 = o1.toString().length();
+                int len2 = o2.toString().length();
+                int lim = Math.min(len1, len2);
+                char[] v1 = o1.toString().toCharArray();
+                char[] v2 = o2.toString().toCharArray();
+
+                int k = 0;
+                while (k < lim) {
+                    char c1 = v1[k];
+                    char c2 = v2[k];
+                    if (c1 != c2) {
+                        return (c1 - c2) * -1;
+                    }
+                    k++;
+                }
+                return len1 - len2;
             }
         });
         temp.addAll(this.depsSet);
-        for (Department dep : this.depsSet) {
+        for (Department dep : temp) {
             res.append(dep.toString() + System.lineSeparator());
         }
         return res.toString();
