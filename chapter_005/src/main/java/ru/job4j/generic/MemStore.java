286afa0e8c49f2741a52be682aac0b46e5dc14a4
alexander-chashkov <asdsacha@mail.ru>
2020-06-02 21:10:22 +0500

    5.2.2. Реализовать Store<T extends Base> [#119452] Исправление 1

--- chapter_005/src/main/java/ru/job4j/generic/MemStore.java
+++ chapter_005/src/main/java/ru/job4j/generic/MemStore.java
@@ -18,18 +18,31 @@ public final class MemStore<T extends Base> implements Store<T> {
         mem.add(model);
     }
 
+    private int findIndexById(String id) {
+        var idx = -1;
+        var i = idx;
+        for (var m : mem) {
+            i++;
+            if (m.getId().equals(id)) {
+                idx = i;
+                break;
+            }
+        }
+        return idx;
+    }
+
     @Override
     public boolean replace(String id, T model) {
-        return mem.set(Integer.parseInt(id), model).getId().equals(model.getId());
+        return mem.set(findIndexById(id), model) == model;
     }
 
     @Override
     public boolean delete(String id) {
-        return mem.remove(Integer.parseInt(id)) != null;
+        return mem.remove(findIndexById(id)) != null;
     }
 
     @Override
     public T findById(String id) {
-        return mem.get(Integer.parseInt(id));
+        return mem.get(findIndexById(id));
     }
 }
--- chapter_005/src/test/java/ru/job4j/generic/RoleStoreTest.java
+++ chapter_005/src/test/java/ru/job4j/generic/RoleStoreTest.java
@@ -19,29 +19,30 @@ public class RoleStoreTest {
     public void initTest() {
         rs = new RoleStore();
         rs.add(new Role("role1"));
+        rs.add(new Role("role5"));
         rs.add(new Role("role2"));
     }
 
     @Test
     public void whenAdd2ElThen2El() {
-        Role role0 = rs.findById("0");
-        Role role1 = rs.findById("1");
+        Role role1 = rs.findById("role1");
+        Role role2 = rs.findById("role2");
         var expected = "role1role2";
-        assertEquals(role0.getId() + role1.getId(), expected);
+        assertEquals(role1.getId() + role2.getId(), expected);
     }
 
     @Test
     public void whenAdd2ElAndReplaceElThenRole3() {
-        rs.replace("0", new Role("role3"));
-        Role role3 = rs.findById("0");
-        rs.delete("1");
+        rs.replace("role1", new Role("role3"));
+        Role role3 = rs.findById("role3");
+        rs.delete("role2");
         var expected = "role3";
         assertEquals(role3.getId(), expected);
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenAdd2ElAndDeleteElThen1El() {
-        rs.delete("1");
-        rs.findById("1");
+        rs.delete("role1");
+        rs.findById("role1");
     }
 }
