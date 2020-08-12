6276165c8726e77ce6f9d27c5c9626d8888e7161
alexander-chashkov <asdsacha@mail.ru>
2020-08-12 23:17:40 +0500

    5. Создать программу 'Консольный чат'. [#119480]

--- chapter_005/src/main/java/ru/job4j/io/Chat.java
+++ chapter_005/src/main/java/ru/job4j/io/Chat.java
@@ -15,11 +15,33 @@ import java.util.Scanner;
  * @since 06.08.2020
  */
 public class Chat {
-    private static final String pathLog = "./chapter_005/src/main/java/ru/job4j/io/chat/log.log";
-    private static final String pathPhrases = "./chapter_005/src/main/java/ru/job4j/io/chat/phrases.txt";
     private static final List<String> phrases = new ArrayList<>();
+    private static final List<String> logMsg = new ArrayList<>();
 
-    private static void initPhrases() {
+    private enum Commands {
+        CONTINUE("ПРОДОЛЖИТЬ"), STOP("СТОП"), EXIT("ЗАКОНЧИТЬ"), UNKNOWN("НЕИЗВЕСТНО");
+
+        private String description;
+
+        Commands(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public static Commands fromValue(String name) {
+            for (final Commands com : values()) {
+                if (com.description.equalsIgnoreCase(name)) {
+                    return com;
+                }
+            }
+            return UNKNOWN;
+        }
+    }
+
+    private static void initPhrases(String pathPhrases) {
         try (BufferedReader read = new BufferedReader(new FileReader(pathPhrases))) {
             read.lines().forEach(line -> {
                 phrases.add(line);
@@ -29,42 +51,51 @@ public class Chat {
         }
     }
 
-    public static void main(String[] args) {
-        initPhrases();
+    private static String getRandomPhrase() {
+        return phrases.get((1 + (int) (Math.random() * phrases.size())) - 1);
+    }
+
+    private static void runChat(String pathLog) {
         boolean hasExit = false;
         boolean hasStop = false;
         Scanner in = new Scanner(System.in);
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(pathLog))) {
-            while (!hasExit) {
-                String str = "";
-                if (in.hasNextLine())
-                    str = in.nextLine();
-                if (!str.trim().isEmpty()) {
-                    switch (str.trim().toUpperCase()) {
-                        case "ПРОДОЛЖИТЬ":
-                            hasStop = false;
-                            break;
-                        case "СТОП":
-                            hasStop = true;
-                            break;
-                        case "ЗАКОНЧИТЬ":
-                            hasExit = true;
-                            break;
-                        default:
-                    }
-                    out.println(str);
-                    if (!hasStop && !hasExit) {
-                        int index = (1 + (int) (Math.random() * phrases.size())) - 1;
-                        System.out.println(phrases.get(index));
-                        out.println(phrases.get(index));
-                    }
+        while (!hasExit) {
+            String str = "";
+            if (in.hasNextLine())
+                str = in.nextLine();
+            if (!str.trim().isEmpty()) {
+                switch (Commands.fromValue(str.trim().toUpperCase())) {
+                    case CONTINUE:
+                        hasStop = false;
+                        break;
+                    case STOP:
+                        hasStop = true;
+                        break;
+                    case EXIT:
+                        hasExit = true;
+                        break;
+                    default:
+                }
+                logMsg.add(str);
+                if (!hasStop && !hasExit) {
+                    String rndStr = getRandomPhrase();
+                    System.out.println(rndStr);
+                    logMsg.add(rndStr);
                 }
             }
+        }
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(pathLog))) {
+            logMsg.stream().forEach(s -> out.println(s));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            initPhrases(args[1]);
+            runChat(args[0]);
+        }
+    }
 }
 
